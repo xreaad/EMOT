0ec1bbcb2760d73700fe2ecc2976ce0d1c795605
xreaad <ryadhchalbi0@gmail.com>
2020-09-15 20:25:51 -0500

    snippet

---
+++ snippet.py
@@ -0,0 +1,27 @@
+import argparse
+
+def search(args):
+    args = args.lower()
+    arguments = args.split()
+    with open('.src/emosnippet', 'r') as f:
+        for line in f:
+            line = line.lower()
+            emoji, names = line.split()[0].lower(), line.split()[1:]
+            for name in names:
+                if name in arguments:
+                    for word in arguments:
+                        if word == name:
+                            args = args.replace(word, emoji)
+
+            
+    print(args)
+
+def main():
+    parser = argparse.ArgumentParser(description='i will get u  what u want from emojis :) ')
+    parser.add_argument('sentence', help='sentence u wanna to translate it to emoji put the sentence between quoets " "', type=str)
+    args = parser.parse_args()
+    search(args.sentence)
+
+
+if __name__ == '__main__':
+    main()
