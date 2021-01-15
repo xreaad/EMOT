a3fcda6fd272592a73aad7f788e72ba3a4bd8bac
xreaad <ryadhchalbi0@gmail.com>
2021-01-15 16:58:42 -0600

    :rocket:

--- snippet.py
+++ snippet.py
@@ -1,23 +1,29 @@
 import argparse
 
-def search(args):
-    args = args.lower()
-    arguments = args.split()
+def search(arg):
+    arg = arg.lower()
+    arguments = arg.split()
     with open('.src/emosnippet', 'r') as f:
         for line in f:
             line = line.lower()
-            emoji, names = line.split()[0].lower(), line.split()[1:]
-            
-    print(args)
+            emoji, name = line.split()[0].lower(), line.split()[1]
+            if name in arguments:
+                for word in arguments:
+                    if word == name:
+                        arg = arg.replace(word, emoji)
+    print(arg)
 
 def translate_file():
     pass
 
 def main():
     parser = argparse.ArgumentParser(description='i will get u  what u want from emojis :) ')
-    parser.add_argument('sentence', help='sentence u wanna to translate it to emoji put the sentence between quoets " "', type=str)
+    parser.add_argument('s', help='sentence u wanna to translate it to emoji put the sentence between quoets " "', type=str)
+    parser.add_argument('-f', '--file', help='translate a file to emoji', action='store_true')
+    parser.add_argument('-t', help='file to translate, take the full file path ', type=str)
+    parser.add_argument('-o', '--output', help='output file after translate', type=str)
     args = parser.parse_args()
-    search(args.sentence)
+    search(args.s)
 
 
 if __name__ == '__main__':
